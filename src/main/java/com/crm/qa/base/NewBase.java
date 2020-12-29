13abe09597b1ee7da0a147fe78ca402466206ab0
sushmita <susmitha.rakapali@gmail.com>
2020-12-28 22:46:26 -0800

    changes from branch1

--- src/main/java/com/crm/qa/base/NewBase.java
+++ src/main/java/com/crm/qa/base/NewBase.java
@@ -3,7 +3,7 @@ package com.crm.qa.base;
 public class NewBase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// This changes are from Branch 1 by Sush
 
 	}
 
